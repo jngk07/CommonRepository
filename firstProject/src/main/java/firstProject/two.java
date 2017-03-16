568fc04ce0be68d2e2f191b50362589f7f074886
karthikeyan <jngk07@gmail.com>
2017-03-16 13:01:46 +0530

    two

---
+++ firstProject/src/main/java/firstProject/two.java
@@ -0,0 +1,10 @@
+package firstProject;
+
+public class two {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("two");
+    }
+
+}
