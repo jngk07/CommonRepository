9f92f273483a0364e1a3bc54de4801d9bf36c5e8
karthikeyan <jngk07@gmail.com>
2017-03-20 14:54:01 +0530

    commit

--- firstProject/src/main/java/firstProject/one.java
+++ firstProject/src/main/java/firstProject/one.java
@@ -1,10 +1,43 @@
 package firstProject;
 
+import java.util.*;
+
 public class one {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("first");
-    }
+    public static int[] arrayLeftRotation(int[] a, int n, int k) {
+        int count =0;
+        for(int i=0;i <k;i++)
+        {
+              int temp = a[0]; // 1
+
+              for(int j=1; j< n; j++){
+
+                  a[count]= a[j]; 
+                  count++;
+           
+              }
+             a[n-1]= temp;
+             count=0;
+         }
+       return a;
+   }
+   
+   public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int n = in.nextInt();
+       int k = in.nextInt();
+       int a[] = new int[n];
+       for(int a_i=0; a_i < n; a_i++){
+           a[a_i] = in.nextInt();
+       }
+     
+       int[] output = new int[n];
+       output = arrayLeftRotation(a, n, k);
+       for(int i = 0; i < n; i++)
+           System.out.print(output[i] + " ");
+     
+       System.out.println();
+     
+   }
 
 }
